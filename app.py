6d09207b3fb33fb3a29d7ab3873158312bf83790
HenriqueStafocher <henriquesystem6@hotmail.com>
2026-05-06 16:52:31 -0300

    correção do banco de dados

--- app.py
+++ app.py
@@ -9,16 +9,18 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Configurações do MySQL usando os.getenv
-# app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-# app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-# app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-# app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 12688)) # A porta do Aiven é importante
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'ijkl30015'
-app.config['MYSQL_DB'] = 'brecho'
+# Configuração SSL OBRIGATÓRIA para o Aiven
+app.config['MYSQL_CUSTOM_OPTIONS'] = {
+    'ssl': {
+        'ca': os.path.join(os.path.dirname(__file__), 'ca.pem')
+    }
+}
 mysql = MySQL(app)
 
 # Login Admnistrador
